df654be3ae9826c2cd165c6120b1ad1a223568db
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-10-22 08:06:56 -0700

    addDoc usecases
    if collection name present each time the docs will be created with new autogenerated docid to that same already present collection and if the collection name is not present then each time the autogenerated ids new documents will be created to a new same collection of its name

--- app.js
+++ app.js
@@ -6,23 +6,26 @@ import {
     collection,
     addDoc,
     increment
-} from "./firebase"
+} from "./firebase.js"
 
 
 const list = document.getElementById("list")
 
-const addTodo = () => {
+const addTodo = async () => {
     const todo = document.getElementById("todo");
 
-    let ref = collection(db, "todos");
-    addDoc(ref, {
+    let ref = collection(db, "billbord");
+    await addDoc(ref, {
         id: increment(1),
         todo: todo.value
     })
 
-        list.innerHTML += `<li>${todo.value}</li>`
-    console.log("-> todo.value", todo.value)
+    // list.innerHTML += `<li>${todo.value}</li>`
+    // console.log("-> todo.value", todo.value)
+    console.log("Todo added successfully")
+    todo.value = "";
 }
+alert("Firebase Firestore connected successfully")
 
 const addTodoBtn = document.getElementById("addTodoBtn")
 addTodoBtn.addEventListener("click", addTodo)
