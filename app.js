7797f3831dcf59da922ea9f697ecfcdf1f2a0f4e
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-10-22 07:15:45 -0700

    collection, addDoc , increment
    firebase methods

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+import {
+    db,
+    doc,
+    setDoc,
+    getDoc,
+    collection,
+    addDoc,
+    increment
+} from "./firebase"
+
+
 const list = document.getElementById("list")
 
 const addTodo = () => {
-const todo = document.getElementById("todo");
-list.innerHTML += `<li>${todo.value}</li>`
+    const todo = document.getElementById("todo");
 
-console.log("-> todo.value", todo.value)
+    let ref = collection(db, "todos");
+    addDoc(ref, {
+        id: increment(1),
+        todo: todo.value
+    })
+
+        list.innerHTML += `<li>${todo.value}</li>`
+    console.log("-> todo.value", todo.value)
 }
 
 const addTodoBtn = document.getElementById("addTodoBtn")
